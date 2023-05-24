a2721fd68a6a73713874759d87281093ef28bc48
franskter1ful <frankster1ful@gmail.com>
2023-05-24 14:11:14 -0500

    Initial frontend App

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Images Gallery</h1>
+//     </div>
+//   );
+// }
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>
+      Testing modern arrow function
+    </h1>
+    </div>
+  );
+}
+
+export default App;
