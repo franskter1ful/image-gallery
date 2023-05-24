b5090a7a64001e14dbde062a3417abd83b50a786
franskter1ful <frankster1ful@gmail.com>
2023-05-24 15:56:06 -0500

    changed favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Images Gallery</h1>
-//     </div>
-//   );
-// }
-
-
-const App = () => {
+function App() {
   return (
     <div className="App">
-      <h1>
-      Testing modern arrow function 2
-    </h1>
+      <h1>Images Gallery</h1>
     </div>
   );
 }
+
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <h1>
+//       Testing modern arrow function 2
+//     </h1>
+//     </div>
+//   );
+// }
 
 export default App;
