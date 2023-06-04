ce8f6e71e27277129933046db31465f01cd03aa7
franskter1ful <frankster1ful@gmail.com>
2023-06-04 01:25:45 -0600

    add card component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../src/components/Header';
 import Search from '../src/components/Search';
+import ImageCard from '../src/components/ImageCard';
 //import _default from "react-bootstrap/esm/CardColumns";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  //console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -17,7 +21,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
@@ -29,6 +33,7 @@ const App = () => {
     <div>
       <Header title="Mezzanine Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <ImageCard />
     </div>
   );
 };
